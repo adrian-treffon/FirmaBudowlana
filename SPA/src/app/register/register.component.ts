3ba92e2abf72568ab98eb4e55459b8c5cc252c26
szymon-trybala <szymon.trybala@protonmail.com>
2019-05-29 02:12:38 +0200

    Initial commit: added skeleton of login & register

---
+++ SPA/src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
